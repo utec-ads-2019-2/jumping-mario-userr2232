9fc1dc19517454cbe45ac13ae17728d08c669a24
userr2232 <reynaldo.rz.26@gmail.com>
2019-08-25 16:04:14 -0500

    done

--- main.cpp
+++ main.cpp
@@ -3,6 +3,20 @@
 using namespace std;
 
 int main() {
-	return EXIT_SUCCESS;
+	int TC;
+	cin >> TC;
+	for(int i = 1; i <= TC; ++i){
+		int highJumps = 0, lowJumps = 0;
+		int walls, wall, currentWall;
+		cin >> walls; cin >> currentWall; --walls;
+		while(walls--) {
+			cin >> wall;
+			if(currentWall > wall) ++lowJumps;
+			if(currentWall < wall) ++highJumps;
+			currentWall = wall;
+		}
+		cout << "Case " << i << ": " << highJumps << " " << lowJumps << endl;
+	}
+	return 0;
 }
 
